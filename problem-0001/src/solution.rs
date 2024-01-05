2cb4651fa1a4dabd342be66a0469a013ba499163
eren <erik.raik.engelhardt@gmail.com>
2024-01-05 15:11:51 +0100

    Implemented fast solution for problem 1

--- problem-0001/src/solution.rs
+++ problem-0001/src/solution.rs
@@ -1,4 +1,5 @@
 use crate::custom_error::LCError;
+use std::collections::HashMap;
 
 #[tracing::instrument]
 pub fn process(nums: Vec<i32>, target: i32) -> miette::Result<Vec<i32>, LCError> {
@@ -13,6 +14,24 @@ pub fn process(nums: Vec<i32>, target: i32) -> miette::Result<Vec<i32>, LCError>
         std::io::ErrorKind::InvalidInput,
         format!("No solution found for 'nums' = '{nums:?}, 'target' = '{target}'"),
     )))
+}
+
+#[tracing::instrument]
+pub fn process_2(nums: Vec<i32>, target: i32) -> miette::Result<Vec<i32>, LCError> {
+    let summands: HashMap<i32, usize> = nums
+        .iter()
+        .enumerate()
+        .map(|(i, num)| (num - target, i))
+        .collect();
+
+    let (index_1, index_2) = nums
+        .iter()
+        .enumerate()
+        .filter_map(|(index_1, num)| summands.get(&(-num)).map(|index_2| (index_1, *index_2)))
+        .find(|(index_1, index_2)| index_1 != index_2)
+        .unwrap();
+
+    Ok(vec![index_1 as i32, index_2 as i32])
 }
 
 #[cfg(test)]
@@ -33,6 +52,21 @@ mod tests {
         let nums = nums.to_vec();
         let expexted = expected.to_vec();
         assert_eq!(expexted, process(nums, target)?);
+        Ok(())
+    }
+    #[rstest]
+    #[case([2, 7, 11, 15].to_vec(), 9, [0, 1].to_vec())]
+    #[case([3,2,4].to_vec(), 6, [1, 2].to_vec())]
+    #[case([3,3].to_vec(), 6, [0, 1].to_vec())]
+    #[tracing::instrument]
+    fn test_process_2(
+        #[case] nums: Vec<i32>,
+        #[case] target: i32,
+        #[case] expected: Vec<i32>,
+    ) -> miette::Result<()> {
+        let nums = nums.to_vec();
+        let expexted = expected.to_vec();
+        assert_eq!(expexted, process_2(nums, target)?);
         Ok(())
     }
 }
